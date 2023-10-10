b1f3ab1ffc805633157c50c060bd49e3406c20cb
Jagan Ganesh <HeyItsJagan@icloud.com>
2023-10-10 22:47:52 +0530

    11. Data Types

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let firstName = "Jagan";
-let lastName = "Ganesh";
-let profession = "Programmer";
-let years = 8;
+/*
+    11. Data Types
+    In JavaScript value is neither Object or Primitive.
+    JavaScript has Dynamic Typing Language.
 
-const PI = 3.14;
+    Primitive Data Types
+    1. Numbers
+    2. String
+    3. Boolean
+    4. Undefined
+    5. Null
+    6. Symbols (ES2015)
+    7. BigInt (ES2020)
+*/
 
-console.log(firstName, lastName, PI);
+let age = 29;
+let userName = "Jagan Ganesh";
+let hasJob = true;
+let children;
+
+console.log(typeof age, age);
+console.log(typeof userName, userName);
+console.log(typeof hasJob, hasJob);
+console.log(typeof children, children);
