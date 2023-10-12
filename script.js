7f31aec7f6a4883829bf08fc18babd0b5e926ebf
Jagan Ganesh <HeyItsJagan@icloud.com>
2023-10-12 19:40:40 +0530

    Coding Challenge #2

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 /*
-    18. Taking Decisions: if / else Statements
+    Coding Exercise 2
 */
 
-const age = 19;
-const userName = "Jackie";
-const isOldEnough = 18;
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
-if(age > isOldEnough) {
-    console.log(`${userName} is old enough to take license.`);
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 } else {
-    console.log(`${userName} is too young, wait another ${isOldEnough - age} years.`);
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
